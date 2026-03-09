a1551e766561e247b9b4684bca34adef04bbe313
Eric Levy <ericmlevy@gmail.com>
2026-03-09 17:17:10 -0400

    preset update

--- tunet.py
+++ tunet.py
@@ -557,7 +557,7 @@ class MainWindow(QMainWindow):
         presets = {
             "Beauty / Paint Fix": {
                 "model_type": "msrn",
-                "model_size_dims": "64",
+                "model_size_dims": "128",
                 "resolution": "512",
                 "overlap_factor": "0.5",
                 "loss": "l1+lpips",
